f256ac28ed30200cdd85eda8e9860aca2005c05d
Hritik Singh <ghritiksingh21@gmail.com>
2025-07-12 15:22:37 +0530

    Version 12

--- GitTutorial/src/Main.java
+++ GitTutorial/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         Change change=new Change();
 
-        System.out.println("Version1");
+        System.out.println("Version12");
         System.out.println(change.str);
 
 
